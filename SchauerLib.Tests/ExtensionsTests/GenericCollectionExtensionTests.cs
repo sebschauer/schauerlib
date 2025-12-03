98688ba4fbd6c7b01e93cb990aab8054bf6a4a7a
sebschauer <github@sebschauer.de>
2025-12-03 14:17:38 +0100

    added FirstOf

---
+++ SchauerLib.Tests/ExtensionsTests/GenericCollectionExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿using SchauerLib.Extensions;
+
+namespace SchauerLib.Tests.ExtensionsTests;
+
+public class GenericCollectionExtensionTests
+{
+    public class FirstOfShould()
+    {
+        [Fact]
+        public void ReturnFirstOccurence()
+        {
+            var (baseClass, subA1, subA2, subB) = (new BaseClass(), new SubClassA(), new SubClassA(), new SubClassB());
+            var list = new List<BaseClass> { baseClass, subA1, subA2, subB };
+
+            Assert.Equal(subA1, list.FirstOf<SubClassA>());
+            Assert.Equal(subB, list.FirstOf<SubClassB>());
+
+            var list2 = new List<BaseClass> { subB, baseClass };
+            Assert.Equal(subB, list2.FirstOf<BaseClass>());
+        }
+
+        [Fact]
+        public void ReturnNullWhenNoMatch()
+        {
+            var (baseClass, subA1, subA2) = (new BaseClass(), new SubClassA(), new SubClassA());
+            var list = new List<BaseClass> { baseClass, subA1, subA2 };
+
+            Assert.Null(list.FirstOf<SubClassB>());
+        }
+    }
+
+    class BaseClass { }
+    class SubClassA : BaseClass { }
+    class SubClassB : BaseClass { }
+
+}
