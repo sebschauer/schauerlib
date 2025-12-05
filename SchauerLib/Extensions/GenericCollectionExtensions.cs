d3f18a894a3705ce6b64ad33011550118cfea75d
sebschauer <github@sebschauer.de>
2025-12-05 10:15:34 +0100

    documentation

--- SchauerLib/Extensions/GenericCollectionExtensions.cs
+++ SchauerLib/Extensions/GenericCollectionExtensions.cs
@@ -2,8 +2,26 @@
 
 namespace SchauerLib.Extensions
 {
+    /// <summary>
+    /// Extension methods for IEnumerable, IList, ICollection, ...
+    /// </summary>
     public static class GenericCollectionExtensions
     {
+        /// <summary>
+        /// Returns the first element of the specified type <c>TElement</c> in <c>collection</c>, 
+        /// casted to <c>TElement</c>, if it exists; otherwise <c>null</c>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// var (car, bike, ship) = (new Car(), new Bicycle(), new Ship());
+        /// var list = [ car, ship, car, bike, ship, bike ];
+        /// list.FirstOf<Bicycle>()
+        ///     .DriveForestPath();
+        /// </code>
+        /// </example>
+        /// <typeparam name="TElement">The searched type</typeparam>
+        /// <param name="collection">The list to search for</param>
+        /// <returns>The first match or null</returns>
         public static TElement? FirstOf<TElement>(this IEnumerable collection)
             where TElement : class 
         {
--- SchauerLib/Extensions/StringExtensions.cs
+++ SchauerLib/Extensions/StringExtensions.cs
@@ -2,14 +2,24 @@
 
 namespace SchauerLib.Extensions
 {
+    /// <summary>
+    /// Extension methods for strings
+    /// </summary>
     public static class StringExtensions
     {
-        public static string Without(this string? input, params string?[] delete) =>
-            input is null 
+        /// <summary>
+        /// Deletes all occurences of all <c>needles</c> entries in <c>haystack</c>. 
+        /// Ignores <c>null</c> and empty values.
+        /// </summary>
+        /// <param name="haystack">The Haystack</param>
+        /// <param name="needles">The needles</param>
+        /// <returns>The haystack without all needles</returns>
+        public static string Without(this string? haystack, params string?[] needles) =>
+            haystack is null 
             ? null 
-            : delete is null
-                ? input
-                : delete.Aggregate(input, (cur, next) => 
+            : needles is null
+                ? haystack
+                : needles.Aggregate(haystack, (cur, next) => 
                     string.IsNullOrEmpty(next) 
                         ? cur 
                         : cur.Replace(next, ""));
