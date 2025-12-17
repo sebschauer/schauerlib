4cc1f3613c3d7f1ff3f783fc3cbb4bc3195d23cf
sebschauer <154711432+sebschauer@users.noreply.github.com>
2025-12-17 21:48:31 +0100

    use net8 (#9)

--- SchauerLib.Extensions/GenericElementExtensions.cs
+++ SchauerLib.Extensions/GenericElementExtensions.cs
@@ -18,34 +18,33 @@
 
 ﻿using System.Linq;
 
-namespace SchauerLib.Extensions
+namespace SchauerLib.Extensions;
+
+/// <summary>
+/// Extension methods for single generic objects
+/// </summary>
+public static class GenericElementExtensions
 {
     /// <summary>
-    /// Extension methods for single generic objects
+    /// Returns true, if <c>element</c> is one of the candidates
     /// </summary>
-    public static class GenericElementExtensions
-    {
-        /// <summary>
-        /// Returns true, if <c>element</c> is one of the candidates
-        /// </summary>
-        /// <example>
-        /// Example:
-        /// <code>
-        /// var input = Console.ReadLine();
-        /// foreach(var c in input)
-        /// {
-        ///     if (c.IsOneOf('-', ' ', '.', 'w', 'a', 's', 'd')
-        ///     {
-        ///         //...
-        ///     }
-        /// }
-        /// </code>
-        /// </example>
-        /// <typeparam name="T">The type of the element and candidates</typeparam>
-        /// <param name="element">The element to compare</param>
-        /// <param name="candidates">The candidates to search for equality</param>
-        /// <returns>True if <c>element</c> is found in <c>candidates</c>, otherwise <c>null</c>.</returns>
-        public static bool IsOneOf<T>(this T element, params  T[] candidates) =>
-            candidates.Contains(element);
-    }
+    /// <example>
+    /// Example:
+    /// <code>
+    /// var input = Console.ReadLine();
+    /// foreach(var c in input)
+    /// {
+    ///     if (c.IsOneOf('-', ' ', '.', 'w', 'a', 's', 'd')
+    ///     {
+    ///         //...
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
+    /// <typeparam name="T">The type of the element and candidates</typeparam>
+    /// <param name="element">The element to compare</param>
+    /// <param name="candidates">The candidates to search for equality</param>
+    /// <returns>True if <c>element</c> is found in <c>candidates</c>, otherwise <c>null</c>.</returns>
+    public static bool IsOneOf<T>(this T element, params  T[] candidates) =>
+        candidates.Contains(element);
 }
